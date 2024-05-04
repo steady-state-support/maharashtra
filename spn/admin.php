46b6f03dc2ab6fb2a1c3c9595181fa674c3d6ffe
sridhar.naidu <sridhar.naidu@extreme-spartan.com>
2024-05-04 14:15:17 +0530

    Deployed e8aa56c with MkDocs version: 1.4.3

---
+++ spn/admin.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Smart IoT Vehicles for Securing High Value Assets &#8212; WordPress</title>
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='dns-prefetch' href='//stats.wp.com' />
+<script src="https://www.smartcarsmart.com/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.14.0" id="regenerator-runtime-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/dist/hooks.min.js?ver=2810c76e705dd1a53b18" id="wp-hooks-js"></script>
+<script src="https://stats.wp.com/w.js?ver=202418" id="woo-tracks-js"></script>
+<link rel='stylesheet' id='dashicons-css' href='https://www.smartcarsmart.com/wp-includes/css/dashicons.min.css?ver=6.5.2' media='all' />
+<link rel='stylesheet' id='buttons-css' href='https://www.smartcarsmart.com/wp-includes/css/buttons.min.css?ver=6.5.2' media='all' />
+<link rel='stylesheet' id='forms-css' href='https://www.smartcarsmart.com/wp-admin/css/forms.min.css?ver=6.5.2' media='all' />
+<link rel='stylesheet' id='l10n-css' href='https://www.smartcarsmart.com/wp-admin/css/l10n.min.css?ver=6.5.2' media='all' />
+<link rel='stylesheet' id='login-css' href='https://www.smartcarsmart.com/wp-admin/css/login.min.css?ver=6.5.2' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://www.smartcarsmart.com/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="admin" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<div style="margin: 5px 0 20px;">
+				<p style="font-size: 14px;">
+					Prove your humanity				</p>
+				<br/>
+				<label for="jetpack_protect_answer" style="vertical-align:super;">
+					1 &nbsp; + &nbsp; 8 &nbsp; = &nbsp;				</label>
+				<input type="number" id="jetpack_protect_answer" name="jetpack_protect_num" value="" size="2" style="width:50px;height:25px;vertical-align:middle;font-size:13px;" class="input" />
+				<input type="hidden" name="jetpack_protect_answer" value="89815d822331f00d22618021991498bd7b74b2e1" />
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://www.smartcarsmart.com/wp-admin/admin.php?page=stats&amp;noheader&amp;proxy&amp;chart=admin-bar-hours-scale" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://www.smartcarsmart.com/my-account/lost-password/">Lost your password?</a>			</p>
+			<script>
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_pass" ); d.value = "";d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+</script>
+		<p id="backtoblog">
+			<a href="https://www.smartcarsmart.com/">&larr; Go to Smart IoT Vehicles for Securing High Value Assets</a>		</p>
+			</div>
+			<script id="essential-blocks-blocks-localize-js-extra">
+var eb_conditional_localize = {"editor_type":""};
+var EssentialBlocksLocalize = {"eb_plugins_url":"https:\/\/www.smartcarsmart.com\/wp-content\/plugins\/essential-blocks\/","image_url":"https:\/\/www.smartcarsmart.com\/wp-content\/plugins\/essential-blocks\/assets\/images","eb_wp_version":"6.5","eb_version":"4.5.11","eb_admin_url":"https:\/\/www.smartcarsmart.com\/wp-admin\/","rest_rootURL":"https:\/\/www.smartcarsmart.com\/wp-json\/","ajax_url":"https:\/\/www.smartcarsmart.com\/wp-admin\/admin-ajax.php","nft_nonce":"d5e7fa2e4d","post_grid_pagination_nonce":"6963e88d48","placeholder_image":"https:\/\/www.smartcarsmart.com\/wp-content\/plugins\/essential-blocks\/assets\/images\/placeholder.png","is_pro_active":"false","upgrade_pro_url":"https:\/\/essential-blocks.com\/upgrade","responsiveBreakpoints":{"tablet":1024,"mobile":767}};
+</script>
+<script src="https://www.smartcarsmart.com/wp-content/plugins/essential-blocks/assets/js/eb-blocks-localize.js?ver=4.5.11" id="essential-blocks-blocks-localize-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"https:\/\/www.smartcarsmart.com\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/dist/i18n.min.js?ver=5e580eb46a90c2b997e6" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+</script>
+<script src="https://www.smartcarsmart.com/wp-admin/js/password-strength-meter.min.js?ver=6.5.2" id="password-strength-meter-js"></script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src="https://www.smartcarsmart.com/wp-includes/js/wp-util.min.js?ver=6.5.2" id="wp-util-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"fb75f3425c"};
+</script>
+<script src="https://www.smartcarsmart.com/wp-admin/js/user-profile.min.js?ver=6.5.2" id="user-profile-js"></script>
+	</body>
+	</html>
+	
